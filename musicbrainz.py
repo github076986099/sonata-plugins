46dadba869c83d487f7872ca4954e94c59e99543
chrysn <chrysn@fsfe.org>
2009-05-06 19:43:48 +0200

    general cleanup, split ui from plugin object

--- musicbrainz.py
+++ musicbrainz.py
@@ -56,6 +56,7 @@ import musicbrainz2.model as mb_mod
 # my preferred way of developing plugins
 from plugin_class import Plugin
 
+
 ############################################# might even be useful in misc
 class html(unicode):
     """just like a unicode object, but will escape all arguments to % formatting"""
@@ -71,15 +72,100 @@ def get_hyperlink(o, label):
     label = label or (o.getTitle() if hasattr(o, 'getTitle') else o.getName())
     return html('<a href="%s.html">%s</a>')%(o.getId(), label)
 
-class PseudoResult(object):
-    """Exception that behaves like an artist / a release / an album"""
-    def __init__(self, message):
-        self.message = message
-    def get_id(self):
-        return hash(self)
+
+############################################# the tab
+class MusicBrainzDisplay(gtk.VBox):
+    """GTK widget for inclusion in a tab. Should not be bothered with sonata
+    internals or sonata plugin API issues. Will happily work together with what
+    the musicbrainz API provides."""
+
+    # define how relations should be displayed
+    relation_type2label = {
+            u'http://musicbrainz.org/ns/rel-1.0#Discogs': _('Discogs'),
+            u'http://musicbrainz.org/ns/rel-1.0#IMDb': _('IMDb'),
+            u'http://musicbrainz.org/ns/rel-1.0#AmazonAsin': _('Amazon'), # FIXME: albums w/ two asins should have a better ui for that
+            u'http://musicbrainz.org/ns/rel-1.0#Wikipedia': _('Wikipedia'), # FIXME: show language in link
+            u'http://musicbrainz.org/ns/rel-1.0#OfficialHomepage': _('home page'),
+            u'http://musicbrainz.org/ns/rel-1.0#Fanpage': _('fan page'), # FIXME: can be many
+            u'http://musicbrainz.org/ns/rel-1.0#Myspace': _('myspace'),
+            u'http://musicbrainz.org/ns/rel-1.0#DownloadForFree': _('download for free'), # useless for this song, but there might be more
+            u'http://musicbrainz.org/ns/rel-1.0#PurchaseForDownload': _('purchase'),
+            u'http://musicbrainz.org/ns/rel-1.0#Musicmoz': _('MusicMoz'),
+            u'http://musicbrainz.org/ns/rel-1.0#Discography': _('discography'),
+            u'http://musicbrainz.org/ns/rel-1.0#Review': _('review'),
+            u'http://musicbrainz.org/ns/rel-1.0#Youtube': _('youtube'),
+            u'http://musicbrainz.org/ns/rel-1.0#OnlineCommunity': _('online community'), # FIXME: give more detailed information in link (stripped version of url)
+            u'http://musicbrainz.org/ns/rel-1.0#Blog': _('blog'),
+            # cummulative FIXME:
+            # * for non-1:1 relations, there's a need for more detailed display
+            #   (have seen numerous fan pages, they should go by url in an own
+            #   section)
+            # * didn't find a comprehensive list, that's what showed up when
+            #   going through parts of my music
+            # * (here for first occurrence): sooner or later (rather later),
+            #   plugins will want to bring their own gettext translations; for
+            #   now, _(x) == x will be a sane default
+           }
+
+    def __init__(self):
+        super(MusicBrainzDisplay, self).__init__()
+
+        self.label = sexy.UrlLabel()
+        self.label.props.use_markup = True
+        self.label.connect('url_activated', self.on_link_clicked)
+        self.label.set_alignment(0, 0)
+
+        self.pack_start(self.label, expand=False)
+
+    def set_fetching(self):
+        self.label.set_markup(_("Fetching musicbrainz data..."))
+
+    def set_empty(self):
+        self.label.set_markup(_("No current song."))
+
+    def set_data(self, mb_data):
+        # will manipulate data
+        mb_data = mb_data.copy()
+
+        # don't show duplicate information about artist and album artist
+        if 'albumartist' in mb_data and 'artist' in mb_data and mb_data['albumartist'].getId() == mb_data['artist'].getId():
+            mb_data['bothartist'] = mb_data['albumartist']
+            del mb_data['albumartist']
+            del mb_data['artist']
+
+        infos = []
+        for key, label in [
+                ('album', _('Album')),
+                ('albumartist', _('Album artist')),
+                ('bothartist', _('Artist, album artist')),
+                ('artist', _('Artist')),
+                ('track', _('Track')),
+                ]:
+            if key in mb_data:
+                current_name = mb_data[key].getTitle() if hasattr(mb_data[key], 'getTitle') else mb_data[key].getName() # function name differs for tracks / releases / artists
+
+                links = [get_hyperlink(mb_data[key], _('MusicBrainz'))]
+                logging.info("Got %d relations for %s", len(mb_data[key].getRelations()), key)
+                for rel in mb_data[key].getRelations():
+                    logging.debug(vars(rel))
+                    if rel.getType() in self.relation_type2label:
+                        links.append(html('<a href="%s">%s</a>')%(rel.getTargetId(), self.relation_type2label[rel.getType()]))
+                    else:
+                        links.append(html(u"%s → %s"%(rel.getType(), rel.getTargetId())))
+                        logging.warning("No information on how to display relations of type %r", rel.getType())
+
+                infos.append(html('<b>%s:</b> %s')%(label, current_name) + ' <small>(%s)</small>'%(", ".join(links),))
+
+        self.label.set_markup('\n'.join(infos))
+
+    @staticmethod
+    def on_link_clicked(widget, url):
+        webbrowser.open(url)
 
 ############################################# the plugin
 class MusicBrainzPlugin(Plugin):
+    ############################### object maintenance (for now, only logging. might do things like opening and closing the musicbrainz cache once there is one)
+
     def __init__(self):
         logging.info('creating new MusicBrainzPLugin instance')
 
@@ -95,127 +181,53 @@ class MusicBrainzPlugin(Plugin):
     def on_construct_tab(self):
         logging.info('construct_tab called')
 
-        vbox = gtk.VBox()
-        self.label = sexy.UrlLabel()
-        self.label.props.use_markup = True
-        self.label.connect('url_activated', self.on_link_clicked)
-        self.label.set_alignment(0, 0)
-        # ... don't know what might be added here later
-        vbox.pack_start(self.label, expand=False)
-        vbox.show_all()
+        self.display = MusicBrainzDisplay()
+        self.display.show_all()
 
         # means new_tab(page, stock, text, focus)
-        return (vbox, None, _("MusicBrainz"), None)
-        # FIXME (here for first occurrence): sooner or later (rather later),
-        # plugins will want to bring their own gettext translations; for now,
-        # _(x) == x will be a sane default
+        return (self.display, None, _("MusicBrainz"), None)
 
     def on_song_change(self, songinfo):
-        logging.info('on_song_change called, %r', songinfo)
-
         if songinfo:
-            self.mb_data = self._extract_mb_data(songinfo)
-
-            # help  in development
-            logging.info("song changed")
-            for k,v in sorted(songinfo.items()):
-                logging.info("\t %s %r", k,v)
+            self.display.set_fetching()
+            mb_data = self._extract_mb_data(songinfo)
+            self.display.set_data(mb_data)
         else:
-            self.mb_data = {}
-
-        self._update_display()
+            self.display.set_empty()
 
     def on_lyrics_fetching(self, callback, artist, title):
         logging.info("on lyrics fetching, %r"%((callback,artist,title),))
-        logging.info("current is %r", self.current)
-        callback(None, "bad info!")
+        callback(None, "bad data, i want songinfo!")
 
     ############################### recurring events
     def _extract_mb_data(self, songinfo):
         """Store the musicbrainz data in the plugin object for easy access, set
         to None if not present"""
 
-        self.current = {
+        ids = {
                 'album': songinfo.get('musicbrainz_albumid', None),
                 'albumartist': songinfo.get('musicbrainz_albumartistid', None),
                 'artist': songinfo.get('musicbrainz_artistid', None),
-                'track': songinfo.get('musicbrainz_trackid', None), # FIXME: for regular songs, this is not passed from mpd. works for cue flac songs, though (tag is set in files' metadata, so it should work)
+                'track': songinfo.get('musicbrainz_trackid', None), # for mp3, starts working after mpd bug #2324 is fixed; after 0.15beta1
                 }
+        # mb library: take all urls you can get
         artist_inc = mb_ws.ArtistIncludes(urlRelations=True)
         release_inc = mb_ws.ReleaseIncludes(urlRelations=True)
         track_inc = mb_ws.TrackIncludes(urlRelations=True)
+
+        mb_data = {}
+
         # FIXME: this is blocking. either find a way to do this w/o blocking, or move into another thread.
         # Moreover, do this in parallel.
-        if self.current['album']:
-            self.current['album'] = mb_ws.Query().getReleaseById(self.current['album'], release_inc)
-        if self.current['track']:
-            self.current['track'] = mb_ws.Query().getTrackById(self.current['track'], track_inc)
-        if self.current['artist']:
-            self.current['artist'] = mb_ws.Query().getArtistById(self.current['artist'], artist_inc)
-        # FIXME: timeout issues when fetching "various artists" (increasing timeout would cause even longer ui lockup, cf last fixme)
+        if ids['album']:
+            mb_data['album'] = mb_ws.Query().getReleaseById(ids['album'], release_inc)
+        if ids['track']:
+            mb_data['track'] = mb_ws.Query().getTrackById(ids['track'], track_inc)
+        if ids['artist']:
+            mb_data['artist'] = mb_ws.Query().getArtistById(ids['artist'], artist_inc)
+        if ids['albumartist']:
+            # FIXME: timeout issues when fetching "various artists" (increasing timeout would cause even longer ui lockup, cf last fixme)
+            if ids['albumartist'] != '89ad4ac3-39f7-470e-963a-56509c546377': # exclude "various artists" for now
+                mb_data['albumartist'] = mb_ws.Query().getArtistById(ids['albumartist'], artist_inc)
 
-        if self.current['albumartist']:
-            if self.current['albumartist'] != '89ad4ac3-39f7-470e-963a-56509c546377': # exclude "various artists" for now
-                self.current['albumartist'] = mb_ws.Query().getArtistById(self.current['albumartist'], artist_inc)
-            else:
-                self.current['albumartist'] = None
-
-    relation_type2label = { # how relations should be displayed
-            u'http://musicbrainz.org/ns/rel-1.0#Discogs': _('Discogs'),
-            u'http://musicbrainz.org/ns/rel-1.0#IMDb': _('IMDb'),
-            u'http://musicbrainz.org/ns/rel-1.0#AmazonAsin': _('Amazon'), # FIXME: albums w/ two asins should have a better ui for that
-            u'http://musicbrainz.org/ns/rel-1.0#Wikipedia': _('Wikipedia'), # FIXME: show language in link
-            u'http://musicbrainz.org/ns/rel-1.0#OfficialHomepage': _('home page'),
-            u'http://musicbrainz.org/ns/rel-1.0#Fanpage': _('fan page'), # FIXME: can be many
-            u'http://musicbrainz.org/ns/rel-1.0#Myspace': _('myspace'),
-            u'http://musicbrainz.org/ns/rel-1.0#DownloadForFree': _('download for free'), # useless for this song, but there might be more
-            u'http://musicbrainz.org/ns/rel-1.0#PurchaseForDownload': _('purchase'),
-            u'http://musicbrainz.org/ns/rel-1.0#Musicmoz': _('MusicMoz'),
-            u'http://musicbrainz.org/ns/rel-1.0#Discography': _('discography'),
-            u'http://musicbrainz.org/ns/rel-1.0#Review': _('review'),
-            u'http://musicbrainz.org/ns/rel-1.0#Youtube': _('youtube'),
-            u'http://musicbrainz.org/ns/rel-1.0#OnlineCommunity': _('online community'), # FIXME: give more detailed information in link
-            u'http://musicbrainz.org/ns/rel-1.0#Blog': _('blog'),
-            # cummulative FIXME:
-            # * for non-1:1 relations, there's a need for more detailed display
-            #   (have seen numerous fan pages, they should go by url in an own
-            #   section)
-            # * didn't find a comprehensive list, that's what showed up when
-            #   going through parts of my music
-           }
-
-    def _update_display(self):
-        """Show information about `current` song in the tab"""
-        current = self.current.copy()
-        if current['albumartist'] and current['artist'] and current['albumartist'].getId() == current['artist'].getId():
-            current['bothartist'] = current['albumartist']
-            del current['albumartist']
-            del current['artist']
-
-        infos = []
-        for key, label in [
-                ('album', _('Album')),
-                ('albumartist', _('Album artist')),
-                ('bothartist', _('Artist, album artist')),
-                ('artist', _('Artist')),
-                ('track', _('Track')),
-                ]:
-            if key in current and current[key]:
-                current_name = current[key].getTitle() if hasattr(current[key], 'getTitle') else current[key].getName()
-
-                links = [get_hyperlink(current[key], _('MusicBrainz'))]
-                logging.info("Got %d relations for %s", len(current[key].getRelations()), key)
-                for rel in current[key].getRelations():
-                    logging.debug(vars(rel))
-                    if rel.getType() in self.relation_type2label:
-                        links.append(html('<a href="%s">%s</a>')%(rel.getTargetId(), self.relation_type2label[rel.getType()]))
-                    else:
-                        links.append(html(u"%s → %s"%(rel.getType(), rel.getTargetId())))
-
-                infos.append(html('<b>%s:</b> %s')%(label, current_name) + ' <small>(%s)</small>'%(", ".join(links),))
-
-        self.label.set_markup('\n'.join(infos))
-
-    @staticmethod
-    def on_link_clicked(widget, url):
-        webbrowser.open(url)
+        return mb_data
